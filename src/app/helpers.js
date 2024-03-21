6d8be6eea93a961614f851194fdd9919a534e6e3
xyzeez <106898953+xyzeez@users.noreply.github.com>
2024-03-21 10:00:30 +0300

    🔨 refactor(tools): updated global functions

--- src/app/helpers.js
+++ src/app/helpers.js
@@ -10,6 +10,72 @@ export const setJSON = (name, data) => {
   localStorage.setItem(name, JSON.stringify(data));
 };
 
-export const clearForm = (inputs) => {
-  inputs.map((input) => (input.value = ''));
+export const validateInput = (input, regex, validatorArray) => {
+  const data = input.value;
+
+  if (!data) {
+    setInputValid(input, false);
+  }
+
+  const valid = regex.test(data);
+
+  if (!valid) setInputValid(input, false);
+
+  validatorArray.push(valid);
+};
+
+export const setInputValid = (input, status) => {
+  input.setAttribute('aria-invalid', !status);
+};
+
+export const clearInputError = (formToClear, validatorArray) => {
+  const form = formToClear;
+
+  const inputs = Array.from(form.querySelectorAll('input'));
+
+  inputs.forEach((input) => setInputValid(input, true));
+
+  validatorArray = [];
+};
+
+export const resetForm = (formID, inputs, validatorArray) => {
+  const form = document.querySelector(formID);
+  const formInputs = Array.from(form.querySelectorAll(inputs));
+
+  clearForm(formInputs, validatorArray);
+};
+
+export const clearForm = (inputs, validatorArray) => {
+  inputs.forEach((input) => (input.value = ''));
+
+  validatorArray = [];
+};
+
+export const convertToSlug = (str) => {
+  return str.trim().toLowerCase().replace(/\s+/g, '-');
+};
+
+export const getHash = () => {
+  const hash = window.location.hash.slice(1);
+
+  if (!hash) return false;
+
+  return hash;
+};
+
+export const clearHash = () => {
+  window.location.hash = '';
+};
+
+export const capitalizeFirstLetter = (str) => {
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+};
+
+export const allTrue = (array) => array.every((element) => element === true);
+
+export const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
 };
