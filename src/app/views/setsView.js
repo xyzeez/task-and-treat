3430b2a70662e8256c24a919c75f4c32a1436b6d
xyzeez <106898953+xyzeez@users.noreply.github.com>
2024-03-23 04:31:32 +0300

    ♻️ refactor(app): reordered function position

--- src/app/views/setsView.js
+++ src/app/views/setsView.js
@@ -46,35 +46,6 @@ class SetsView extends View {
     </form> 
         `;
 
-  _generateMarkup = (user, data) => {
-    return `
-      <section id="sectionStart" class="bg-ashe flex flex-col gap-10 md:gap-14 xl:gap-32 px-4 py-8 w-full max-w-[1300px] mx-auto md:px-8 lg:px-16 xl:rounded-[30px] xl:my-12 xl:py-16">
-        <div class="flex flex-row justify-between">
-          <h1 class="text-4xl md:text-5xl font-semibold">
-            <span class="font-light block text-3xl md:text-4xl">Welcome back,</span>
-            ${capitalizeFirstLetter(user)}!
-          </h1>
-          <div class="flex flex-col items-end gap-4 fixed md:relative md:bottom-0 bottom-[3%] md:right-0 right-[5%] text-white text-base">
-            <button id="btnAddNewSet-1" class="btnAddNewSet bg-black rounded-xl">
-              <svg class="aspect-square w-12 md:w-16 p-2">
-                <use href="${icons}#icon-plus"></use>
-              </svg>
-            </button>
-            <p class="hidden md:block text-black">Add a new Set</p>
-          </div>
-        </div>
-        <div class="grid grid-cols-2 md:grid-cols-3 lg:flex lg:flex-row lg:flex-wrap gap-4">
-          ${this._generateSets(data)}
-          <button id="btnAddNewSet-2" class="btnAddNewSet flex w-full lg:max-w-[280px] box-shadow aspect-[1/0.89] items-center justify-center rounded-2xl min-[426px]:rounded-3xl md:rounded-[30px] bg-[#EAEAEA]">
-            <svg class="aspect-square w-16 sm:w-28 bg-[#DFDFDF] rounded-full p-2 sm:p-6 text-black">
-              <use href="${icons}#icon-plus"></use>
-            </svg>
-          </button>
-        </div>
-      </section>
-                `;
-  };
-
   _generateSets = (data) => {
     let markup = '';
 
@@ -103,6 +74,35 @@ class SetsView extends View {
       .join('');
 
     return markup;
+  };
+
+  _generateMarkup = (user, data) => {
+    return `
+      <section id="sectionStart" class="bg-ashe flex flex-col gap-10 md:gap-14 xl:gap-32 px-4 py-8 w-full max-w-[1300px] mx-auto md:px-8 lg:px-16 xl:rounded-[30px] xl:my-12 xl:py-16">
+        <div class="flex flex-row justify-between">
+          <h1 class="text-4xl md:text-5xl font-semibold">
+            <span class="font-light block text-3xl md:text-4xl">Welcome back,</span>
+            ${capitalizeFirstLetter(user)}!
+          </h1>
+          <div class="flex flex-col items-end gap-4 fixed md:relative md:bottom-0 bottom-[3%] md:right-0 right-[5%] text-white text-base">
+            <button id="btnAddNewSet-1" class="btnAddNewSet bg-black rounded-xl">
+              <svg class="aspect-square w-12 md:w-16 p-2">
+                <use href="${icons}#icon-plus"></use>
+              </svg>
+            </button>
+            <p class="hidden md:block text-black">Add a new Set</p>
+          </div>
+        </div>
+        <div class="grid grid-cols-2 md:grid-cols-3 lg:flex lg:flex-row lg:flex-wrap gap-4">
+          ${this._generateSets(data)}
+          <button id="btnAddNewSet-2" class="btnAddNewSet flex w-full lg:max-w-[280px] box-shadow aspect-[1/0.89] items-center justify-center rounded-2xl min-[426px]:rounded-3xl md:rounded-[30px] bg-[#EAEAEA]">
+            <svg class="aspect-square w-16 sm:w-28 bg-[#DFDFDF] rounded-full p-2 sm:p-6 text-black">
+              <use href="${icons}#icon-plus"></use>
+            </svg>
+          </button>
+        </div>
+      </section>
+                `;
   };
 
   _renderMarkUp = (user, data, update = false) => {
